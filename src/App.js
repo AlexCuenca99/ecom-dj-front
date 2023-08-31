794e697883201218ac8713bd120296804aa5ff98
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-08-31 15:33:07 -0500

    Wrap redux store to the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import { Provider } from 'react-redux';
+import store from 'app/store';
+
 function App() {
 	return (
-		<div className="text-amber-800 underline decoration-dotted">
-			<p>Hola desde App</p>
-		</div>
+		<Provider store={store}>
+			<div className="text-amber-800 underline decoration-dotted">
+				<p>Hola desde App</p>
+			</div>
+		</Provider>
 	);
 }
 
