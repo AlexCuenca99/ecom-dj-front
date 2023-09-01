cb72f7be5d517b76f1f034efe7b926c4cf4750f7
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-01 16:34:09 -0500

    Add routes to sign-up and account activation

--- src/routes/routes.anonym.js
+++ src/routes/routes.anonym.js
@@ -1,3 +1,23 @@
-const routesAnonym = [];
+import { AuthLayout } from 'layouts';
+import { LoginPage } from 'pages/AuthPages';
+import { ActivateAccountPage, SignUpPage } from 'pages/AnonymPage';
+
+const routesAnonym = [
+	{
+		path: '/login',
+		layout: AuthLayout,
+		component: LoginPage,
+	},
+	{
+		path: '/signup',
+		layout: AuthLayout,
+		component: SignUpPage,
+	},
+	{
+		path: '/activate/:uid/:token',
+		layout: AuthLayout,
+		component: ActivateAccountPage,
+	},
+];
 
 export default routesAnonym;
