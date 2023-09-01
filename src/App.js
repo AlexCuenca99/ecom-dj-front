1d672342f486917bd71ebbfb3574bf3f3ef021c2
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-01 11:39:58 -0500

    Add Navigation (routes) component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import React from 'react';
 
 import { Provider } from 'react-redux';
 import store from 'app/store';
+import { Navigation } from 'routes';
 
 function App() {
 	return (
 		<Provider store={store}>
-			<div className="text-amber-800 underline decoration-dotted">
-				<p>Hola desde App</p>
-			</div>
+			<Navigation />
 		</Provider>
 	);
 }
