de80ebbbf0006abb7bc469d38d949697eae7b1e7
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-02 15:24:48 -0500

    Register RTK's

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import authenticationReducer from 'features/authentication/authenticationSlice';
+import { authenticationApiSlice } from 'features/authentication/authenticationApiSlice';
+import { userApiSlice } from 'features/authentication/usersApiSlice';
 
 export default configureStore({
-	reducer: { authentication: authenticationReducer },
+	reducer: {
+		authentication: authenticationReducer,
+		[authenticationApiSlice.reducerPath]: authenticationApiSlice.reducer,
+		[userApiSlice.reducerPath]: userApiSlice.reducer,
+	},
+	middleware: (getDefaultMiddleware) => {
+		return getDefaultMiddleware()
+			.concat(authenticationApiSlice.middleware)
+			.concat(userApiSlice.middleware);
+	},
 });
