04dcc1d5e35c46c62c16552ca2223c6dc654d7a0
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-02 15:24:33 -0500

    Slice for auth features

--- src/features/authentication/authenticationSlice.js
+++ src/features/authentication/authenticationSlice.js
@@ -1,20 +1,27 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-export const authenticationSlice = createSlice({
+const initialState = { isAuth: false, user: null };
+
+const authenticationSlice = createSlice({
 	name: 'authentication',
-	initialState: {
-		isAuth: false,
-	},
+	initialState,
 	reducers: {
-		login: (state) => {
-			state.value = true;
+		login(state, action) {
+			state.isAuth = true;
+			state.user = {
+				first_name: action.payload.first_name,
+				last_name: action.payload.last_name,
+				gender: action.payload.gender,
+				birth: action.payload.birth,
+				email: action.payload.email,
+			};
 		},
-		logout: (state) => {
-			state.value = false;
+		logout(state) {
+			state.isAuth = false;
+			state.user = null;
 		},
 	},
 });
 
 export const { login, logout } = authenticationSlice.actions;
-
 export default authenticationSlice.reducer;
