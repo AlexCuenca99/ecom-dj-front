42e09980bd334aaf3a179093eeb4cc129e129bd4
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-02 18:46:49 -0500

    Call login form

--- src/pages/AuthPages/LoginPage.js
+++ src/pages/AuthPages/LoginPage.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
+import { LoginForm } from 'features/authentication/components';
+
 export function LoginPage() {
-	return (
-		<div>
-			<h1>LoginPage</h1>
-		</div>
-	);
+	return <LoginForm />;
 }
