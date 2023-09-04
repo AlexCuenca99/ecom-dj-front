2bc5eeba8fbc3b77421f290b57c5f8d6494f5b24
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-04 16:19:09 -0500

    Return null and navigate to home when user is authenticated

--- src/pages/AuthPages/LoginPage.js
+++ src/pages/AuthPages/LoginPage.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 import { LoginForm } from 'features/authentication/components';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 export function LoginPage() {
+	const auth = useSelector((state) => state.authentication);
+
+	const navigate = useNavigate();
+
+	if (auth.isAuth) {
+		navigate('/');
+		return null;
+	}
+
 	return <LoginForm />;
 }
