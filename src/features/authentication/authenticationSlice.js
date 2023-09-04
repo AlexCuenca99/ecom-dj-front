6628986f83fec775368855887abbf56b92952eb9
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-04 13:32:44 -0500

    Set tokens when login

--- src/features/authentication/authenticationSlice.js
+++ src/features/authentication/authenticationSlice.js
@@ -1,13 +1,19 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { ACCESS_TOKEN, REFRESH_TOKEN } from 'utils/constanst';
 
-const initialState = { isAuth: false, user: null };
+const initialState = { isAuth: false, user: null, tokens: [] };
 
 const authenticationSlice = createSlice({
 	name: 'authentication',
 	initialState,
 	reducers: {
 		login(state, action) {
+			// Set tokens in local storage
+			action.payload?.access &&
+				localStorage.setItem(ACCESS_TOKEN, action.payload.access);
+			action.payload?.refresh &&
+				localStorage.setItem(REFRESH_TOKEN, action.payload.refresh);
+
 			state.isAuth = true;
 			state.user = {
 				first_name: action.payload.first_name,
@@ -16,12 +22,21 @@ const authenticationSlice = createSlice({
 				birth: action.payload.birth,
 				email: action.payload.email,
 			};
-			localStorage.setItem(ACCESS_TOKEN, action.payload.access);
-			localStorage.setItem(REFRESH_TOKEN, action.payload.refresh);
+			state.tokens = [
+				{
+					access: localStorage.getItem(ACCESS_TOKEN),
+					refresh: localStorage.getItem(ACCESS_TOKEN),
+				},
+			];
 		},
 		logout(state) {
+			// Set tokens in local storage
+			localStorage.removeItem(ACCESS_TOKEN);
+			localStorage.removeItem(REFRESH_TOKEN);
+
 			state.isAuth = false;
 			state.user = null;
+			state.tokens = [];
 		},
 	},
 });
