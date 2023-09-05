606a4f17ebbc16df105957b641973bad9749c411
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-05 12:41:09 -0500

    Send props to auth nav item

--- src/components/Navigation/NavBar/NavBar.js
+++ src/components/Navigation/NavBar/NavBar.js
@@ -276,7 +276,11 @@ export function NavBar() {
 									to="/login"
 									className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
 								>
-									{auth.isAuth ? <AuthNavMenu /> : 'Login'}
+									{auth.isAuth ? (
+										<AuthNavMenu auth={auth} />
+									) : (
+										'Login'
+									)}
 								</Link>
 							</div>
 						</div>
