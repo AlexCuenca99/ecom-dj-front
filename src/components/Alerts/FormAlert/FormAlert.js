b7045f9324196371c533005f3efc35bb79d9a048
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-05 12:31:04 -0500

    Add transition for alert

--- src/components/Alerts/FormAlert/FormAlert.js
+++ src/components/Alerts/FormAlert/FormAlert.js
@@ -1,41 +1,46 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Transition } from '@headlessui/react';
+
+import { transitionClasses } from 'utils/transitions';
 
 export function FormAlert(props) {
-	const { title, message, type } = props;
+	const { title, message, type, showAlert } = props;
 
 	return (
-		<div
-			role="alert"
-			className={`rounded border-s-4 ${
-				type === 'error'
-					? 'border-red-500'
-					: type === 'success'
-					? 'border-green-500'
-					: ''
-			} bg-red-50 p-4`}
-		>
-			<strong
-				className={`block font-medium ${
+		<Transition show={showAlert} as={Fragment} {...transitionClasses}>
+			<div
+				role="alert"
+				className={`rounded border-s-4 ${
 					type === 'error'
-						? ' text-red-800'
+						? 'border-red-500 bg-red-50 p-4'
 						: type === 'success'
-						? 'text-green-800'
+						? 'border-green-500 bg-green-50 p-4'
 						: ''
 				}`}
 			>
-				{title}
-			</strong>
-			<p
-				className={`mt-2 text-sm ${
-					type === 'error'
-						? 'text-red-700'
-						: type === 'sucess'
-						? 'text-green-700'
-						: ''
-				}`}
-			>
-				{message}
-			</p>
-		</div>
+				<strong
+					className={`block font-medium ${
+						type === 'error'
+							? ' text-red-800'
+							: type === 'success'
+							? 'text-green-800'
+							: ''
+					}`}
+				>
+					{title}
+				</strong>
+				<p
+					className={`mt-2 text-sm ${
+						type === 'error'
+							? 'text-red-700'
+							: type === 'success'
+							? 'text-green-700'
+							: ''
+					}`}
+				>
+					{message}
+				</p>
+			</div>
+		</Transition>
 	);
 }
