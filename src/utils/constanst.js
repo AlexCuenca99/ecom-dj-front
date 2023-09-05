2b3272596a1e3ca9c93f65b6ef99e93d3f0046db
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-05 11:02:14 -0500

    Add general alert titles

--- src/utils/constanst.js
+++ src/utils/constanst.js
@@ -1,2 +1,5 @@
 export const ACCESS_TOKEN = 'access';
 export const REFRESH_TOKEN = 'refresh';
+
+export const ERROR_ALERT_TITLE = 'Something went wrong';
+export const SUCCESS_ALERT_TITLE = 'All went good';
