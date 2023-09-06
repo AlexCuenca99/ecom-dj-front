14153b32cf2a0298d2ff0171c54e6fcd04dc6a10
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-06 18:59:06 -0500

    Register category slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,18 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import authenticationReducer from 'features/authentication/authenticationSlice';
-import { authenticationApiSlice } from 'features/authentication/authenticationApiSlice';
 import { userApiSlice } from 'features/authentication/usersApiSlice';
+import { authenticationApiSlice } from 'features/authentication/authenticationApiSlice';
+import { categoryApiSlice } from 'features/categories/redux/categoriesApiSlice';
 
 export default configureStore({
 	reducer: {
 		authentication: authenticationReducer,
 		[authenticationApiSlice.reducerPath]: authenticationApiSlice.reducer,
 		[userApiSlice.reducerPath]: userApiSlice.reducer,
+		[categoryApiSlice.reducerPath]: categoryApiSlice.reducer,
 	},
 	middleware: (getDefaultMiddleware) => {
 		return getDefaultMiddleware()
 			.concat(authenticationApiSlice.middleware)
-			.concat(userApiSlice.middleware);
+			.concat(userApiSlice.middleware)
+			.concat(categoryApiSlice.middleware);
 	},
 });
