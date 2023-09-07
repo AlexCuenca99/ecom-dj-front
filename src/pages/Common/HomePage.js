4752034064446ed5ddf661a4978dd084732ad80c
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-06 21:07:30 -0500

    Call categories api request

--- src/pages/Common/HomePage.js
+++ src/pages/Common/HomePage.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useListParentCategoriesQuery } from 'features/categories/redux/categoriesApiSlice';
 
 export function HomePage() {
+	const { data } = useListParentCategoriesQuery();
+
+	console.log(data);
 	return (
 		<div>
 			<h1>HomePage</h1>
