a36204f59b6c2c0a9e94c6c191211dd094403999
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-09 10:49:23 -0500

    Add base component

---
+++ src/features/products/components/BestSelling/BestSellingCards/BestSellingCards.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function BestSellingCards() {
+	return (
+		<div>
+			<h1>BestSellingCards</h1>
+		</div>
+	);
+}
--- src/features/products/components/BestSelling/BestSellingCards/index.js
+++ src/features/products/components/BestSelling/BestSellingCards/index.js
@@ -1,2 +1,1 @@
-export * from './BestSellingCard';
 export * from './BestSellingCards';
