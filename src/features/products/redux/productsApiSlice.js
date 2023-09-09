9e0b37d879252b2e41001def8db4a4f8be6c71a5
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-08 20:30:50 -0500

    Add api slice for list products

---
+++ src/features/products/redux/productsApiSlice.js
@@ -0,0 +1,36 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const productApiSlice = createApi({
+	reducerPath: 'productApi',
+	baseQuery: fetchBaseQuery({
+		baseUrl: `${process.env.REACT_APP_BASE_API_URL}/api/v1/`,
+		prepareHeaders: (headers) => {
+			headers.set('Content-Type', 'application/json');
+			headers.set('Accept', 'application/json');
+			return headers;
+		},
+	}),
+	tagTypes: ['Product'],
+	endpoints: (builder) => ({
+		listProducts: builder.query({
+			query({
+				limit = '',
+				offset = '',
+				search = '',
+				price__gte = '',
+				price__gt = '',
+				price__lte = '',
+				price__lt = '',
+				category = '',
+				ordering = '',
+			}) {
+				return {
+					url: `products/?limit=${limit}&offset=${offset}&search=${search}&price__gte=${price__gte}&price__gt=${price__gt}&price__lte=${price__lte}&price__lt=${price__lt}&category=${category}&ordering=${ordering}`,
+					method: 'GET',
+				};
+			},
+		}),
+	}),
+});
+
+export const { useListProductsQuery } = productApiSlice;
