a2808de1320f26b2b7383a7f2e4c863088dea7c0
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-09 10:31:25 -0500

    Add base component

---
+++ src/components/Banners/BannerPromo/BannerPromo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function BannerPromo() {
+	return (
+		<div>
+			<h1>BannerPromo</h1>
+		</div>
+	);
+}
--- src/components/Banners/BannerPromo/index.js
+++ src/components/Banners/BannerPromo/index.js
@@ -1,2 +1,1 @@
-export * from './HomeBanner';
 export * from './BannerPromo';
