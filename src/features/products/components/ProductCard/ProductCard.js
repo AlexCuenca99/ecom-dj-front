55d924bddfdce2fc27fd4ee992e152b85c5193b2
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-08 20:30:32 -0500

    Render product requests results

--- src/features/products/components/ProductCard/ProductCard.js
+++ src/features/products/components/ProductCard/ProductCard.js
@@ -1,21 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export function ProductCard(props) {
 	const { product } = props;
 
 	return (
-		<a key={product.id} href={product.href} className="group">
+		<Link to={`products/${product.id}`} className="group">
 			<div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
 				<img
-					src={product.imageSrc}
-					alt={product.imageAlt}
+					src={product.photo}
+					alt={product.name}
 					className="h-full w-full object-cover object-center group-hover:opacity-75"
 				/>
 			</div>
 			<h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
 			<p className="mt-1 text-lg font-medium text-gray-900">
-				{product.price}
+				$ {product.price}
 			</p>
-		</a>
+		</Link>
 	);
 }
