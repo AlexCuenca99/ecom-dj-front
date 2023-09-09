b67b4422e578e953cc2dd10f116c209e670e3e49
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-08 20:29:57 -0500

    Register product RTK

--- src/app/store.js
+++ src/app/store.js
@@ -4,6 +4,7 @@ import authenticationReducer from 'features/authentication/authenticationSlice';
 import { userApiSlice } from 'features/authentication/usersApiSlice';
 import { authenticationApiSlice } from 'features/authentication/authenticationApiSlice';
 import { categoryApiSlice } from 'features/categories/redux/categoriesApiSlice';
+import { productApiSlice } from 'features/products/redux/productsApiSlice';
 
 export default configureStore({
 	reducer: {
@@ -11,11 +12,13 @@ export default configureStore({
 		[authenticationApiSlice.reducerPath]: authenticationApiSlice.reducer,
 		[userApiSlice.reducerPath]: userApiSlice.reducer,
 		[categoryApiSlice.reducerPath]: categoryApiSlice.reducer,
+		[productApiSlice.reducerPath]: productApiSlice.reducer,
 	},
 	middleware: (getDefaultMiddleware) => {
 		return getDefaultMiddleware()
 			.concat(authenticationApiSlice.middleware)
 			.concat(userApiSlice.middleware)
-			.concat(categoryApiSlice.middleware);
+			.concat(categoryApiSlice.middleware)
+			.concat(productApiSlice.middleware);
 	},
 });
