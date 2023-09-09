ffb91bcaf8bb409089551932f0b6cb67436b14c2
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-09 11:29:24 -0500

    Render products by created and modified

--- src/pages/Common/HomePage.js
+++ src/pages/Common/HomePage.js
@@ -1,81 +1,48 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import { HomeBanner, BannerPromo } from 'components';
-import { ProductsCards } from 'features/products/components';
+import { BannerPromo } from 'components';
+import { ProductsCards, BestSellingCards } from 'features/products/components';
 
 import { useListProductsQuery } from 'features/products/redux/productsApiSlice';
 
-const preProducts = [
-	{
-		id: 1,
-		name: 'Earthen Bottle',
-		href: '#',
-		price: '$48',
-		imageSrc:
-			'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg',
-		imageAlt:
-			'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
-	},
-	{
-		id: 2,
-		name: 'Nomad Tumbler',
-		href: '#',
-		price: '$35',
-		imageSrc:
-			'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg',
-		imageAlt:
-			'Olive drab green insulated bottle with flared screw lid and flat top.',
-	},
-	{
-		id: 3,
-		name: 'Focus Paper Refill',
-		href: '#',
-		price: '$89',
-		imageSrc:
-			'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
-		imageAlt:
-			'Person using a pen to cross a task off a productivity paper card.',
-	},
-	{
-		id: 4,
-		name: 'Machined Mechanical Pencil',
-		href: '#',
-		price: '$35',
-		imageSrc:
-			'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
-		imageAlt:
-			'Hand holding black machined steel mechanical pencil with brass tip and top.',
-	},
-	// More products...
-];
-
 export function HomePage() {
-	const [limit, setLimit] = useState(4);
-	const [offset, setOffset] = useState('');
-	const [search, setSearch] = useState('');
-	const [priceGte, setPriceGte] = useState('');
-	const [priceGt, setPriceGt] = useState('');
-	const [priceLte, setPriceLte] = useState('');
-	const [priceLt, setPriceLt] = useState('');
-	const [category, setCategory] = useState('');
-	const [ordering, setOrdering] = useState('created');
+	// const [limit, setLimit] = useState(4);
+	// const [offset, setOffset] = useState('');
+	// const [search, setSearch] = useState('');
+	// const [priceGte, setPriceGte] = useState('');
+	// const [priceGt, setPriceGt] = useState('');
+	// const [priceLte, setPriceLte] = useState('');
+	// const [priceLt, setPriceLt] = useState('');
+	// const [category, setCategory] = useState('');
+	// const [ordering, setOrdering] = useState('created');
 
 	const {
-		data: products,
+		data: arrivalsProducts,
 		isLoading,
 		isError,
 		isFetching,
-		currentData,
 	} = useListProductsQuery({
-		limit,
-		offset,
-		search,
-		priceGte,
-		priceGt,
-		priceLte,
-		priceLt,
-		category,
-		ordering,
+		limit: 4,
+		offset: '',
+		search: '',
+		priceGte: '',
+		priceGt: '',
+		priceLte: '',
+		priceLt: '',
+		category: '',
+		ordering: 'created',
+	});
+
+	const { data: bestSellingProducts } = useListProductsQuery({
+		limit: 3,
+		offset: '',
+		search: '',
+		priceGte: '',
+		priceGt: '',
+		priceLte: '',
+		priceLt: '',
+		category: '',
+		ordering: '-sold',
 	});
 
 	return (
@@ -89,7 +56,8 @@ export function HomePage() {
 				btnAction={'/'}
 				images={[]}
 			/>
-			<ProductsCards products={products?.results} />
+			<ProductsCards products={arrivalsProducts?.results} />
+			<BestSellingCards products={bestSellingProducts?.results} />
 		</>
 	);
 }
