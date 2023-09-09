eade39f7cf5131e4d611339b3b3f5632da379654
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-09 10:37:27 -0500

    Render props

--- src/components/Banners/BannerPromo/BannerPromo.js
+++ src/components/Banners/BannerPromo/BannerPromo.js
@@ -1,19 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-export function BannerPromo() {
+export function BannerPromo(props) {
+	const { mainText, subText, btnTitle, btnAction, images } = props;
+
 	return (
 		<div className="relative bg-white overflow-hidden">
 			<div className="pt-16 pb-80 sm:pt-24 sm:pb-40 lg:pt-40 lg:pb-48">
 				<div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 sm:static">
 					<div className="sm:max-w-lg">
 						<h1 className="text-4xl font font-extrabold tracking-tight text-gray-900 sm:text-6xl">
-							Summer styles are finally here
+							{mainText}
 						</h1>
-						<p className="mt-4 text-xl text-gray-500">
-							This year, our new summer collection will shelter
-							you from the harsh elements of a world that doesn't
-							care if you live or die.
-						</p>
+						<p className="mt-4 text-xl text-gray-500">{subText}</p>
 					</div>
 					<div>
 						<div className="mt-10">
@@ -83,12 +82,12 @@ export function BannerPromo() {
 								</div>
 							</div>
 
-							<a
-								href="#"
+							<Link
+								to={btnAction}
 								className="inline-block text-center bg-indigo-600 border border-transparent rounded-md py-3 px-8 font-medium text-white hover:bg-indigo-700"
 							>
-								Shop Collection
-							</a>
+								{btnTitle}
+							</Link>
 						</div>
 					</div>
 				</div>
