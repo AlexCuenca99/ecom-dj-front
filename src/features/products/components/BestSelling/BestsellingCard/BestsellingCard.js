6e7fcf5d1b34582c0ef8b5d6b5bb4f30149672c5
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-09 10:48:04 -0500

    Style card component

--- src/features/products/components/BestSelling/BestsellingCard/BestsellingCard.js
+++ src/features/products/components/BestSelling/BestsellingCard/BestsellingCard.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
-export function BestsellingCard() {
+export function BestSellingCard(props) {
+	const { product } = props;
+
 	return (
-		<div>
-			<h1>BestsellingCard</h1>
+		<div key={product.id} className="group relative">
+			<div className="w-full h-96 rounded-lg overflow-hidden group-hover:opacity-75 sm:h-auto sm:aspect-w-2 sm:aspect-h-3">
+				<img
+					src={product.imageSrc}
+					alt={product.imageAlt}
+					className="w-full h-full object-center object-cover"
+				/>
+			</div>
+			<h3 className="mt-4 text-base font-semibold text-gray-900">
+				<a href={product.href}>
+					<span className="absolute inset-0" />
+					{product.name}
+				</a>
+			</h3>
+			<p className="mt-1 text-sm text-gray-500">{product.price}</p>
 		</div>
 	);
 }
