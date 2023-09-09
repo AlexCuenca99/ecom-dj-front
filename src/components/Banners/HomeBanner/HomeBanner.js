440dcfca683f71ef509a56cdcc77e0acfa6905aa
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-08 19:09:33 -0500

    Add base home banner component

---
+++ src/components/Banners/HomeBanner/HomeBanner.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function HomeBanner() {
+	return (
+		<div>
+			<h1>HomeBanner</h1>
+		</div>
+	);
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export * from './Navigation';
 export * from './Alerts';
+export * from './Banners';
