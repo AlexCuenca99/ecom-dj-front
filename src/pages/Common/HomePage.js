aa38e70ede73e5274b3e1a31d517f54cb0b0348d
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-08 19:12:51 -0500

    Call home banner

--- src/pages/Common/HomePage.js
+++ src/pages/Common/HomePage.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
+import { HomeBanner } from 'components';
+
 export function HomePage() {
 	return (
-		<div>
+		<>
+			<HomeBanner
+				mainText={'Take control of your team.'}
+				subText={
+					'Start building for free, then add a site plan to go live. Account plans unlock additional features.'
+				}
+				sectionTitle={'Pricing'}
+			/>
 			<h1>Home Page</h1>
-		</div>
+		</>
 	);
 }
