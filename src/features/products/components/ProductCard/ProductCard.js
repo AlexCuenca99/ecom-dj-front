55eb40560a25cf9dd6493cbb18f30b0bae4c1bee
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-08 19:25:43 -0500

    Style product card and pass props

--- src/features/products/components/ProductCard/ProductCard.js
+++ src/features/products/components/ProductCard/ProductCard.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
-export function ProductCard() {
+export function ProductCard(props) {
+	const { product } = props;
+
 	return (
-		<div>
-			<h1>ProductCard</h1>
-		</div>
+		<a key={product.id} href={product.href} className="group">
+			<div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+				<img
+					src={product.imageSrc}
+					alt={product.imageAlt}
+					className="h-full w-full object-cover object-center group-hover:opacity-75"
+				/>
+			</div>
+			<h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
+			<p className="mt-1 text-lg font-medium text-gray-900">
+				{product.price}
+			</p>
+		</a>
 	);
 }
