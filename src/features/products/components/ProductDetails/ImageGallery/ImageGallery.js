5df6f9de6e1203f902980679f8d870b260ed5502
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-09 13:01:37 -0500

    Style component

--- src/features/products/components/ProductDetails/ImageGallery/ImageGallery.js
+++ src/features/products/components/ProductDetails/ImageGallery/ImageGallery.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-export function ImageGallery() {
+export function ImageGallery(props) {
+	const { product } = props;
+
 	return (
-		<div>
-			<h1>ImageGallery</h1>
+		<div as="div" className="flex flex-col-reverse">
+			<div className="w-full aspect-w-1 aspect-h-1">
+				<img
+					src={product.photo}
+					alt={product.name}
+					className="w-full h-full object-center object-cover sm:rounded-lg"
+				/>
+			</div>
 		</div>
 	);
 }
