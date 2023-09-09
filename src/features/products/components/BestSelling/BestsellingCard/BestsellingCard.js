bd333562e6e04ab37beac44cc19c5f4bb3acd405
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-09 11:07:46 -0500

    Render request results

--- src/features/products/components/BestSelling/BestsellingCard/BestsellingCard.js
+++ src/features/products/components/BestSelling/BestsellingCard/BestsellingCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export function BestSellingCard(props) {
 	const { product } = props;
@@ -7,16 +8,16 @@ export function BestSellingCard(props) {
 		<div key={product.id} className="group relative">
 			<div className="w-full h-96 rounded-lg overflow-hidden group-hover:opacity-75 sm:h-auto sm:aspect-w-2 sm:aspect-h-3">
 				<img
-					src={product.imageSrc}
-					alt={product.imageAlt}
+					src={product.photo}
+					alt={product.name}
 					className="w-full h-full object-center object-cover"
 				/>
 			</div>
 			<h3 className="mt-4 text-base font-semibold text-gray-900">
-				<a href={product.href}>
+				<Link to={`/product/${product.id}`}>
 					<span className="absolute inset-0" />
 					{product.name}
-				</a>
+				</Link>
 			</h3>
 			<p className="mt-1 text-sm text-gray-500">{product.price}</p>
 		</div>
