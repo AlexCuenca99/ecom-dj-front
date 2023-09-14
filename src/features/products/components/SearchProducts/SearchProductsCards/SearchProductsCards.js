1446deeb8834195e504d1780eb297076d248440d
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-13 19:43:50 -0500

    Style and render props

--- src/features/products/components/SearchProducts/SearchProductsCards/SearchProductsCards.js
+++ src/features/products/components/SearchProducts/SearchProductsCards/SearchProductsCards.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { SearchProductsCard } from '../index';
 
-export function SearchProductsCards() {
+export function SearchProductsCards(props) {
+	const { products } = props;
 	return (
-		<div>
-			<h1>SearchProductsCards</h1>
+		<div className="px-4">
+			<ul className="px-4 -my-6 divide-y divide-gray-200">
+				{products &&
+					products.results.map((product) => (
+						<SearchProductsCard
+							key={product.id}
+							product={product}
+						/>
+					))}
+			</ul>
 		</div>
 	);
 }
