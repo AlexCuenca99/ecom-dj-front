f4445c3eb6ea2016232f9b6a0e3276fa675b365c
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-13 19:59:48 -0500

    Remove extra padding

--- src/features/products/components/SearchProducts/SearchProductsCards/SearchProductsCards.js
+++ src/features/products/components/SearchProducts/SearchProductsCards/SearchProductsCards.js
@@ -4,16 +4,11 @@ import { SearchProductsCard } from '../index';
 export function SearchProductsCards(props) {
 	const { products } = props;
 	return (
-		<div className="px-4">
-			<ul className="px-4 -my-6 divide-y divide-gray-200">
-				{products &&
-					products.results.map((product) => (
-						<SearchProductsCard
-							key={product.id}
-							product={product}
-						/>
-					))}
-			</ul>
-		</div>
+		<ul className="px-4 -my-4">
+			{products &&
+				products.results.map((product) => (
+					<SearchProductsCard key={product.id} product={product} />
+				))}
+		</ul>
 	);
 }
