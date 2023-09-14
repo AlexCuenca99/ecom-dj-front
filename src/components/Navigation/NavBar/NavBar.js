eb29073bd12c87b0a140e13564f868120f085eeb
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-13 19:31:51 -0500

    Prevent null products

--- src/components/Navigation/NavBar/NavBar.js
+++ src/components/Navigation/NavBar/NavBar.js
@@ -291,46 +291,55 @@ export function NavBar() {
 
 								<div className="px-4">
 									<ul className="px-4 -my-6 divide-y divide-gray-200">
-										{productsData.results.map((product) => (
-											<li
-												key={product.id}
-												className="flex py-6"
-											>
-												<div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-													<img
-														src={product.photo}
-														alt={product.name}
-														className="h-full w-full object-cover object-center"
-													/>
-												</div>
-
-												<div className="ml-4 flex flex-1 flex-col">
-													<div>
-														<div className="flex justify-between text-base font-medium text-gray-900">
-															<h3>
-																<a
-																	href={
-																		product.href
-																	}
-																>
-																	{
-																		product.name
-																	}
-																</a>
-															</h3>
-															<p className="ml-4">
-																{product.price}
-															</p>
+										{productsData &&
+											productsData.results.map(
+												(product) => (
+													<li
+														key={product.id}
+														className="flex py-6"
+													>
+														<div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+															<img
+																src={
+																	product.photo
+																}
+																alt={
+																	product.name
+																}
+																className="h-full w-full object-cover object-center"
+															/>
 														</div>
-														<p className="mt-1 text-sm text-gray-500">
-															{
-																product.description
-															}
-														</p>
-													</div>
-												</div>
-											</li>
-										))}
+
+														<div className="ml-4 flex flex-1 flex-col">
+															<div>
+																<div className="flex justify-between text-base font-medium text-gray-900">
+																	<h3>
+																		<a
+																			href={
+																				product.href
+																			}
+																		>
+																			{
+																				product.name
+																			}
+																		</a>
+																	</h3>
+																	<p className="ml-4">
+																		{
+																			product.price
+																		}
+																	</p>
+																</div>
+																<p className="mt-1 text-sm text-gray-500">
+																	{
+																		product.description
+																	}
+																</p>
+															</div>
+														</div>
+													</li>
+												)
+											)}
 									</ul>
 								</div>
 
