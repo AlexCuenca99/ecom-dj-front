875af2401960b5a3865133e8adf831b8779577bd
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-13 19:41:28 -0500

    Add base component SearchProductsCards

---
+++ src/features/products/components/SearchProducts/SearchProductsCards/SearchProductsCards.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function SearchProductsCards() {
+	return (
+		<div>
+			<h1>SearchProductsCards</h1>
+		</div>
+	);
+}
--- src/features/products/components/SearchProducts/SearchProductsCards/index.js
+++ src/features/products/components/SearchProducts/SearchProductsCards/index.js
@@ -1,2 +1,1 @@
-export * from './SearchProductsCard';
 export * from './SearchProductsCards';
