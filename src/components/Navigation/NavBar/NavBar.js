6bbde2be061c4bd21a7c00ba7862aaa3e6c3407a
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-13 19:40:07 -0500

    Call Search Product Card

--- src/components/Navigation/NavBar/NavBar.js
+++ src/components/Navigation/NavBar/NavBar.js
@@ -20,6 +20,7 @@ import {
 } from '@heroicons/react/20/solid';
 
 import { AuthNavMenu } from 'features/authentication/components';
+import { SearchProductsCard } from 'features/products/components';
 import { useListParentCategoriesMutation } from 'features/categories/redux/categoriesApiSlice';
 import { useListProductsQuery } from 'features/products/redux/productsApiSlice';
 
@@ -294,50 +295,10 @@ export function NavBar() {
 										{productsData &&
 											productsData.results.map(
 												(product) => (
-													<li
+													<SearchProductsCard
 														key={product.id}
-														className="flex py-6"
-													>
-														<div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-															<img
-																src={
-																	product.photo
-																}
-																alt={
-																	product.name
-																}
-																className="h-full w-full object-cover object-center"
-															/>
-														</div>
-
-														<div className="ml-4 flex flex-1 flex-col">
-															<div>
-																<div className="flex justify-between text-base font-medium text-gray-900">
-																	<h3>
-																		<a
-																			href={
-																				product.href
-																			}
-																		>
-																			{
-																				product.name
-																			}
-																		</a>
-																	</h3>
-																	<p className="ml-4">
-																		{
-																			product.price
-																		}
-																	</p>
-																</div>
-																<p className="mt-1 text-sm text-gray-500">
-																	{
-																		product.description
-																	}
-																</p>
-															</div>
-														</div>
-													</li>
+														product={product}
+													/>
 												)
 											)}
 									</ul>
